1464e96ff46bb9cc1b505689cc61a0af8a2ec0d7
jhstarr <jeffhstarr@gmail.com>
2017-11-05 07:33:55 -0800

    working

--- Name.java
+++ Name.java
@@ -1,33 +1,44 @@
-
-/**
- * Write a description of class Name here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Name
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Name
-     */
-    public Name()
-    {
-        // initialise instance variables
-        x = 0;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String fullName = this.firstName + this.middleName + this.lastName;
+    
+    public Name(String first, String middle, String last) {
+        firstName = first;
+        middleName = middle;
+        lastName = last;
+    }
+    public String getFirst() {
+        return this.firstName;
+    }
+    public String getMiddle() {
+        return this.middleName;
+    }
+    public String getLast() {
+        return this.lastName;
+    }
+    public String firstMiddleLast() {
+        return this.firstName + " " + this.middleName + " " + this.lastName;
+    }
+    public String lastFirstMiddle() {
+        return this.lastName + ", " + this.firstName + " " + this.middleName;
+    }
+    public boolean equals(Name otherName) {
+        if (this.fullName.equalsIgnoreCase(otherName.fullName)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public String initials() {
+        
+    }
+    public int length() {
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
