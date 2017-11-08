c6ca3fc24f7444f9cbe69c34b9030f23f2d9ce1d
annamstarr <31353295+annamstarr@users.noreply.github.com>
2017-11-08 10:04:38 -0800

    working on ch4

--- SoccerTeam.java
+++ SoccerTeam.java
@@ -1,14 +1,14 @@
 public class SoccerTeam {
     public static void main(String[] args) {
-        Team acalanes = new Team("Acalanes Soccer", 20);
+        Team acalanes = new Team("Acalanes Soccer", 10);
         Athlete sam = new Athlete("Sam", 17);
         acalanes.addAthlete(sam,0);
         sam.setAge(23);
         
-        acalanes.addAthlete(sam, 1);
-        System.out.println(acalanes);
         Athlete jill = new Athlete("Sam", 23);
-        System.out.println(sam.equals(jill));
-        System.out.println(jill.equals(sam));
+        acalanes.addAthlete(jill,1);
+        System.out.println(sam.equals(jill));   //will print true because name and age are same
+        
+        System.out.println(acalanes);
     }
 }
